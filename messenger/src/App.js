4962573e815e497c38a73d72f61b4a020e0c335c
Hadas Gayer <carmel.mofet.hadas@gmail.com>
2022-05-22 17:49:33 +0300

    add: rudimentary header component

--- messenger/src/App.js
+++ messenger/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+
+class SubjetTitle extends React.Component {
+  render() {
+    return (
+      <header className='title-header'>
+        {this.props.value}
+      </header>
+    )
+  }
+}
+
+// class MainLogo extends React.Component {
+//   render() {
+//     return (
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           What shall I do with my life?
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     )
+//   }
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          What shall I do with my life?
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='SubjectTitle'>
+        <SubjetTitle value="okayy" />
+      </div>
     </div>
   );
 }
